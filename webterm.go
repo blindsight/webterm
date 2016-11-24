403eac714e0f1c12d406eca66e62a8cb97156c4d
Timothy Rhodes <phoenixevolution@gmail.com>
2016-11-23 22:47:40 -0600

    testing apis are the same.

--- webterm.go
+++ webterm.go
@@ -1,13 +1,26 @@
 package main
 
 import (
-	"time"
-
+	"github.com/gopherjs/gopherjs/js"
 	"honnef.co/go/js/dom"
 )
 
+func paintCanvas() {
+	screen := dom.GetWindow().Document().GetElementByID("screen").(*dom.HTMLCanvasElement)
+
+	context := screen.GetContext2d() //Call("getContext", "2d")
+	context.Font = "38pt Arial"
+	context.FillStyle = "cornflowerblue"
+	context.StrokeStyle = "blue"
+
+	context.Call("fillText", "Hello Canvas", screen.Width/2-150, screen.Height/2-15)
+	context.Call("strokeText", "Hello Canvas", screen.Width/2-150, screen.Height/2-15)
+}
+
 func main() {
-	dom.GetWindow().AddEventListener("DOMContentLoaded", false, func(e dom.Event) {
+	js.Global.Set("paintCanvas", paintCanvas)
+
+	/*dom.GetWindow().AddEventListener("DOMContentLoaded", false, func(e dom.Event) {
 		started := time.Now()
 		screen := dom.GetWindow().Document().GetElementByID("screen").(*dom.HTMLCanvasElement)
 
@@ -19,6 +32,6 @@ func main() {
 		context.Call("fillText", "Hello Canvas", screen.Width/2-150, screen.Height/2-15)
 		context.Call("strokeText", "Hello Canvas", screen.Width/2-150, screen.Height/2-15)
 		println("total time taken is:", time.Since(started).String())
-	})
+	})*/
 
 }
